1313f4c2d0b3c6d896c124c0ac809b3baf65edb8
zori <zorix@users.noreply.github.com>
2024-01-15 19:00:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const port = 3000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get("/", function (req, res) {
+    res.send("GET Hello World!");
+    setInterval(() => {
+        console.log("ping");
+    }, 1000);
+    setTimeout(() => {
+        res.send({status: "GET"});
+    }, 3100);
+});
+
+app.post("/test", function(req, res) {
+    res.send("POST Hello World!");
+    setInterval(() => {
+        console.log("POST ping: " + req.body.test);
+    }, 1000);
+    setTimeout(() => {
+        res.send({status: "POST"});
+    }, 3100);
+});
+
+app.listen(port, function () {
+    console.log(`server listening on port ${port}!`);
+});
